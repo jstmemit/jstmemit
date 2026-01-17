8b12eaf3b7523846327b0ceae1df238115ced040
kamazra4ka <admin@kamazra4ka.nl>
2026-01-17 17:07:55 +0100

    fix: lower stateSize to 1 to produce better results with low amount of saved context

--- src/repositories/MarkovRepository.ts
+++ src/repositories/MarkovRepository.ts
@@ -3,7 +3,7 @@ import pkg, { type MarkovResult } from 'markov-strings';
 const { default: Markov } = pkg;
 
 export class MarkovRepository {
-  public async generateMarkovText(corpus: string[], minLength: number = 2, maxLength: number = 5, stateSize: number = 2): Promise<string> {
+  public async generateMarkovText(corpus: string[], minLength: number = 2, maxLength: number = 5, stateSize: number = 1): Promise<string> {
     const markov = new Markov({
       stateSize,
     });
