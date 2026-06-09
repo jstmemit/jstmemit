a60f1f1452be6bedfec42eeea7341d53f113e64e
kamazra4ka <admin@kamazra4ka.nl>
2026-06-09 22:07:54 +0200

    feat: make FontsRepository.ts with a method to get Impact font buffer

---
+++ apps/meme-generator/src/repositories/FontsRepository.ts
@@ -0,0 +1,11 @@
+import { readFileSync } from "node:fs";
+import { join, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const here: string = dirname(fileURLToPath(import.meta.url));
+
+export class FontsRepository {
+  public getImpact(): Buffer {
+    return readFileSync(join(here, "../data/fonts/Impact.ttf"));
+  }
+}
