f04b384b723ae9cc2c933de7dea345162c5616c8
kamazra4ka <admin@kamazra4ka.nl>
2026-06-10 13:56:31 +0200

    refactor: extract meme command logic from InteractionCreate.ts into MemesController.ts class

---
+++ apps/bot/src/controllers/MemesController.ts
@@ -0,0 +1,40 @@
+import type { IMemesController } from "../interfaces/IMemesController.ts";
+import type { ChatInputCommandInteraction } from "discord.js";
+import type { TemplateResult } from "@jstmemit/meme-generator/models/TemplateResult";
+import { topBottomText } from "@jstmemit/meme-generator/data/templates/topBottomText";
+import type { IMemeService } from "@jstmemit/meme-generator/interfaces/IMemeService";
+
+export class MemesController implements IMemesController {
+  private readonly _memeService: IMemeService;
+
+  public constructor(memeService: IMemeService) {
+    this._memeService = memeService;
+  }
+
+  public async handleMemeInteraction(
+    interaction: ChatInputCommandInteraction,
+  ): Promise<void> {
+    await interaction.deferReply();
+
+    const png: TemplateResult | undefined =
+      await this._memeService.generateMeme(
+        topBottomText,
+        interaction.channelId,
+      );
+
+    if (!png?.result) {
+      await interaction.reply("error");
+      return;
+    }
+
+    await interaction.editReply({
+      content: "123 test",
+      files: [
+        {
+          attachment: png.result,
+          name: "meme.png",
+        },
+      ],
+    });
+  }
+}
--- apps/bot/src/events/InteractionCreate.ts
+++ apps/bot/src/events/InteractionCreate.ts
@@ -2,8 +2,6 @@ import { client } from "../bot.ts";
 import { Events } from "discord.js";
 import { FontsRepository } from "@jstmemit/meme-generator/repositories/FontsRepository";
 import { MemeService } from "@jstmemit/meme-generator/services/MemeService";
-import { topBottomText } from "@jstmemit/meme-generator/data/templates/topBottomText.tsx";
-import type { TemplateResult } from "@jstmemit/meme-generator/models/TemplateResult";
 import type { IFontsRepository } from "@jstmemit/meme-generator/interfaces/IFontsRepository";
 import type { IMemeService } from "@jstmemit/meme-generator/interfaces/IMemeService";
 import type { IMemeRepository } from "@jstmemit/meme-generator/interfaces/IMemeRepository";
@@ -12,41 +10,31 @@ import type { IMessagesRepository } from "@jstmemit/db/interfaces/IMessagesRepos
 import { MessagesRepository } from "@jstmemit/db/repositories/MessagesRepository";
 import type { IImagesRepository } from "@jstmemit/db/interfaces/IImagesRepository";
 import { ImagesRepository } from "@jstmemit/db/repositories/ImagesRepository";
+import type { IMemesController } from "../interfaces/IMemesController.ts";
+import { MemesController } from "../controllers/MemesController.ts";
 
+// repositories
 const fontsRepository: IFontsRepository = new FontsRepository();
 const messagesRepository: IMessagesRepository = new MessagesRepository();
 const memeRepository: IMemeRepository = new MemeRepository(fontsRepository);
 const imagesRepository: IImagesRepository = new ImagesRepository();
+
+// services
 const memeService: IMemeService = new MemeService(
   memeRepository,
   messagesRepository,
   imagesRepository,
 );
 
+// controllers
+const memesController: IMemesController = new MemesController(memeService);
+
 client.on(Events.InteractionCreate, async (interaction): Promise<void> => {
-  if (!interaction.isChatInputCommand()) return;
-
-  if (interaction.commandName === "ping") {
-    await interaction.deferReply();
-
-    const png: TemplateResult | undefined = await memeService.generateMeme(
-      topBottomText,
-      interaction.channelId,
-    );
-
-    if (!png?.result) {
-      await interaction.reply("error");
-      return;
+  // chat commands
+  if (interaction.isChatInputCommand()) {
+    switch (interaction.commandName) {
+      case "meme":
+        await memesController.handleMemeInteraction(interaction);
     }
-
-    await interaction.editReply({
-      content: "123 test",
-      files: [
-        {
-          attachment: png.result,
-          name: "meme.png",
-        },
-      ],
-    });
   }
 });
