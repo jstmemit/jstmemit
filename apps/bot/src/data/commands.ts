4b00c480e78e100c0fd0cb7c225addbf50379780
kamazra4ka <admin@kamazra4ka.nl>
2026-06-10 14:05:07 +0200

    feat: change /ping command name into /meme + add localized name and description

--- apps/bot/src/data/commands.ts
+++ apps/bot/src/data/commands.ts
@@ -1,6 +1,18 @@
 export const commands = [
   {
-    name: "ping",
-    description: "Replies with Pong!",
+    name: "meme",
+    name_localizations: {
+      ru: "мем",
+      uk: "мем",
+    },
+    description: "Generate a meme with a random template",
+    description_localizations: {
+      ru: "Сгенерировать случайный мем",
+      nl: "Genereer een random meme",
+      fr: "Générer un mème aléatoire",
+      de: "Erzeuge ein zufälliges Meme",
+      uk: "Згенерувати рандомний мем",
+      pl: "Wygeneruj losowy mem",
+    },
   },
 ];
