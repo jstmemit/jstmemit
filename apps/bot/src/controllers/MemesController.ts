2d97c70c0d0a640a93f7b3b86d78b05c9f83057a
kamazra4ka <admin@kamazra4ka.nl>
2026-06-13 17:39:02 +0200

    refactor: pass queue and events into MemesController.ts from InteractionCreate.ts

--- apps/bot/src/controllers/MemesController.ts
+++ apps/bot/src/controllers/MemesController.ts
@@ -2,39 +2,38 @@ import type { IMemesController } from "../interfaces/IMemesController.ts";
 import { type ChatInputCommandInteraction } from "discord.js";
 import type { MemeGenerationJob } from "@jstmemit/shared/models/MemeGenerationJob";
 import type { MemeGenerationResult } from "@jstmemit/shared/models/MemeGenerationResult";
-import { createMemeGenerationQueue } from "@jstmemit/queue/jobs/memeGeneration";
-import { createRedisConnection } from "@jstmemit/queue/connection";
-import { type Job, QueueEvents } from "bullmq";
+import type { QueueEvents } from "bullmq";
+import { type Job } from "bullmq";
 import { type Queue } from "bullmq";
-import { type ConnectionOptions } from "bullmq";
-import { Env } from "@jstmemit/shared/schemas/Env";
-
-const env = Env.parse(process.env);
-
-const redisConnection: ConnectionOptions = createRedisConnection(
-  env.REDIS_HOST,
-  env.REDIS_PORT,
-);
-const memeGenerationQueue: Queue<MemeGenerationJob, MemeGenerationResult> =
-  createMemeGenerationQueue(redisConnection);
-const memeGenerationQueueEvents = new QueueEvents("meme-generation", {
-  connection: redisConnection,
-});
 
 export class MemesController implements IMemesController {
+  private readonly _memeGenerationQueue: Queue<
+    MemeGenerationJob,
+    MemeGenerationResult
+  >;
+  private readonly _memeGenerationQueueEvents: QueueEvents;
+
+  public constructor(
+    memeGenerationQueue: Queue<MemeGenerationJob, MemeGenerationResult>,
+    memeGenerationQueueEvents: QueueEvents,
+  ) {
+    this._memeGenerationQueue = memeGenerationQueue;
+    this._memeGenerationQueueEvents = memeGenerationQueueEvents;
+  }
+
   public async handleMemeInteraction(
     interaction: ChatInputCommandInteraction,
   ): Promise<void> {
     await interaction.deferReply();
 
     const job: Job<MemeGenerationJob, MemeGenerationResult> =
-      await memeGenerationQueue.add("meme-generation", {
+      await this._memeGenerationQueue.add("meme-generation", {
         channelId: interaction.channelId,
       });
 
     try {
       const jobResult: MemeGenerationResult = await job.waitUntilFinished(
-        memeGenerationQueueEvents,
+        this._memeGenerationQueueEvents,
         60_000,
       );
       await interaction.editReply({
--- apps/bot/src/events/InteractionCreate.ts
+++ apps/bot/src/events/InteractionCreate.ts
@@ -2,8 +2,35 @@ import { client } from "../bot.ts";
 import { Events } from "discord.js";
 import type { IMemesController } from "../interfaces/IMemesController.ts";
 import { MemesController } from "../controllers/MemesController.ts";
+import type { MemeGenerationJob } from "@jstmemit/shared/models/MemeGenerationJob";
+import type { MemeGenerationResult } from "@jstmemit/shared/models/MemeGenerationResult";
+import { createMemeGenerationQueue } from "@jstmemit/queue/jobs/memeGeneration";
+import { createRedisConnection } from "@jstmemit/queue/connection";
+import { QueueEvents } from "bullmq";
+import { type Queue } from "bullmq";
+import { type ConnectionOptions } from "bullmq";
+import { Env } from "@jstmemit/shared/schemas/Env";
+import type { z } from "zod";
 
-const memesController: IMemesController = new MemesController();
+const env: z.infer<typeof Env> = Env.parse(process.env);
+
+const redisConnection: ConnectionOptions = createRedisConnection(
+  env.REDIS_HOST,
+  env.REDIS_PORT,
+);
+const memeGenerationQueue: Queue<MemeGenerationJob, MemeGenerationResult> =
+  createMemeGenerationQueue(redisConnection);
+const memeGenerationQueueEvents: QueueEvents = new QueueEvents(
+  "meme-generation",
+  {
+    connection: redisConnection,
+  },
+);
+
+const memesController: IMemesController = new MemesController(
+  memeGenerationQueue,
+  memeGenerationQueueEvents,
+);
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 client.on(Events.InteractionCreate, async (interaction): Promise<void> => {
