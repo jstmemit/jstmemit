b6b4c2b3681ca0a8bc8711d6f9b570bd7b1543ac
kamazra4ka <admin@kamazra4ka.nl>
2026-06-16 19:17:58 +0200

    feat: add @jstmemit/analytics package with posthog export

--- packages/shared/src/schemas/Env.ts
+++ packages/shared/src/schemas/Env.ts
@@ -1,11 +1,12 @@
 import { z } from "zod";
 
 export const Env = z.object({
-    DB_FILE_NAME: z.string().min(1),
     DB_URL: z.string().min(1),
 
     DISCORD_TOKEN: z.string().min(1),
     DISCORD_CLIENT_ID: z.string().min(1),
+
+    POSTHOG_WRITE_KEY: z.string().min(1),
 
     REDIS_HOST: z.string().min(1),
     REDIS_PORT: z.coerce.number().min(1),
