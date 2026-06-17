495cbd6ab0e6e3557380b49f5b9cd4501ac926de
kamazra4ka <admin@kamazra4ka.nl>
2026-06-17 13:25:56 +0200

    fix: change "explains" template id to #2 + add 2nd image slot

--- apps/meme-generator/src/data/templates/explains.tsx
+++ apps/meme-generator/src/data/templates/explains.tsx
@@ -1,16 +1,13 @@
-import type { Template } from "../../models/Template.ts";
-import type { TemplateProps } from "../../models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import type { TemplateProps } from "#/models/TemplateProps.ts";
 import * as React from "react";
 
 export const explains: Template = {
-    id: 1,
+    id: 2,
     name: "explains",
     width: 700,
     height: 800,
-    texts: [
-        { id: 0, description: "top text", minLength: 1, maxLength: 5 },
-        { id: 1, description: "bottom text", minLength: 1, maxLength: 5 },
-    ],
+    texts: [],
     images: [{ id: 0, description: "background" }],
     element: ({ images }: TemplateProps) => (
         <div
@@ -24,9 +21,15 @@ export const explains: Template = {
         >
             <img
                 src={images[0]}
-                width={450}
-                height={400}
+                width={455}
+                height={360}
                 style={{ position: "absolute", top: 20, left: 0 }}
+            />
+            <img
+                src={images[0]}
+                width={400}
+                height={320}
+                style={{ position: "absolute", top: 435, left: 0 }}
             />
             <img
                 src="https://raw.githubusercontent.com/jstmemit/jstmemit-bot/refs/heads/master/src/generation/visual/assets/images/explains.png"
@@ -34,12 +37,6 @@ export const explains: Template = {
                 height={800}
                 style={{ position: "absolute", top: 0, left: 0 }}
             />
-            {/*<img*/}
-            {/*  src={images[0]}*/}
-            {/*  width={450}*/}
-            {/*  height={400}*/}
-            {/*  style={{ position: "absolute", top: 0, left: 0, zIndex: 3 }}*/}
-            {/*/>*/}
         </div>
     ),
 };
