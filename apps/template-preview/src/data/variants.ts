eed8b2ffe11bf57043c8d169b37db321f98fcba2
kamazra4ka <admin@kamazra4ka.nl>
2026-06-21 22:32:21 +0200

    feat: shuffle test template data + increase examples amount to 12

--- apps/template-preview/src/data/variants.ts
+++ apps/template-preview/src/data/variants.ts
@@ -1,46 +1,45 @@
 import type { TemplateProps } from "@jstmemit/shared/models/TemplateProps";
+import _ from "lodash";
 
-export const variants: TemplateProps[] = [
-    {
-        texts: ["Cities Skylines 2", "without bicycles"],
-        images: [
-            "https://files.wideunits.nl/memes/examples/city.png",
-            "https://files.wideunits.nl/memes/examples/markrutte.jpeg",
-        ],
-    },
-    {
-        texts: ["KFC", "at 23:59"],
-        images: [
-            "https://files.wideunits.nl/memes/examples/chicken.png",
-            "https://files.wideunits.nl/memes/examples/lightning.png",
-        ],
-    },
-    {
-        texts: ["Game of the year"],
-        images: [
-            "https://files.wideunits.nl/memes/examples/battlefield.png",
-            "https://files.wideunits.nl/memes/examples/hamster.png",
-        ],
-    },
-    {
-        texts: ["", "Sony Headquarters"],
-        images: [
-            "https://files.wideunits.nl/memes/examples/siege.jpg",
-            "https://files.wideunits.nl/memes/examples/ubisoft.jpg",
-        ],
-    },
-    {
-        texts: ["LinusTechTips", "is not coming here"],
-        images: [
-            "https://files.wideunits.nl/memes/examples/applestore.jpg",
-            "https://files.wideunits.nl/memes/examples/linus.png",
-        ],
-    },
-    {
-        texts: ["", "meow"],
-        images: [
-            "https://files.wideunits.nl/memes/examples/cat.png",
-            "https://files.wideunits.nl/memes/examples/rust.jpg",
-        ],
-    },
+// test data for meme template previews
+
+const texts: string[] = [
+    "Cities Skylines 2",
+    "without bicycles",
+    "KFC",
+    "at 23:59",
+    "Game of the year",
+    "Sony Headquarters",
+    "LinusTechTips",
+    "is not coming here",
+    "meow",
+    "aaaaaaaa",
+    "Bot settings",
+    "Mark Rutte",
+    "r/LinusTechTips",
+    "hop on war thunder",
+    "yes",
+    "why",
+    "lol",
+    "پیام آزمایشی",
+    "тестовое сообщение",
 ];
+const images: string[] = [
+    "https://files.wideunits.nl/memes/examples/city.png",
+    "https://files.wideunits.nl/memes/examples/markrutte.jpeg",
+    "https://files.wideunits.nl/memes/examples/chicken.png",
+    "https://files.wideunits.nl/memes/examples/lightning.png",
+    "https://files.wideunits.nl/memes/examples/battlefield.png",
+    "https://files.wideunits.nl/memes/examples/hamster.png",
+    "https://files.wideunits.nl/memes/examples/siege.jpg",
+    "https://files.wideunits.nl/memes/examples/ubisoft.jpg",
+    "https://files.wideunits.nl/memes/examples/applestore.jpg",
+    "https://files.wideunits.nl/memes/examples/linus.png",
+    "https://files.wideunits.nl/memes/examples/cat.png",
+    "https://files.wideunits.nl/memes/examples/rust.jpg",
+];
+
+export const variants: TemplateProps[] = _.times(12, () => ({
+    texts: _.shuffle(texts),
+    images: _.shuffle(images),
+}));
