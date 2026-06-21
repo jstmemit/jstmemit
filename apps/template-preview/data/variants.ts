93d31b2ca31411aae5a51f17dba811cca9d88b84
kamazra4ka <admin@kamazra4ka.nl>
2026-06-21 12:12:11 +0200

    refactor: move rendering template examples logic out of TemplateLayout.astro into Examples.astro server component

---
+++ apps/template-preview/data/variants.ts
@@ -0,0 +1,46 @@
+import type { TemplateProps } from "@jstmemit/shared/models/TemplateProps.ts";
+
+export const variants: TemplateProps[] = [
+    {
+        texts: ["Cities Skylines 2", "without bicycles"],
+        images: [
+            "https://files.wideunits.nl/memes/examples/city.png",
+            "https://files.wideunits.nl/memes/examples/markrutte.jpeg",
+        ],
+    },
+    {
+        texts: ["KFC", "at 23:59"],
+        images: [
+            "https://files.wideunits.nl/memes/examples/chicken.png",
+            "https://files.wideunits.nl/memes/examples/lightning.png",
+        ],
+    },
+    {
+        texts: ["Game of the year"],
+        images: [
+            "https://files.wideunits.nl/memes/examples/battlefield.png",
+            "https://files.wideunits.nl/memes/examples/hamster.png",
+        ],
+    },
+    {
+        texts: ["", "Sony Headquarters"],
+        images: [
+            "https://files.wideunits.nl/memes/examples/siege.jpg",
+            "https://files.wideunits.nl/memes/examples/ubisoft.jpg",
+        ],
+    },
+    {
+        texts: ["LinusTechTips", "is not coming here"],
+        images: [
+            "https://files.wideunits.nl/memes/examples/applestore.jpg",
+            "https://files.wideunits.nl/memes/examples/linus.png",
+        ],
+    },
+    {
+        texts: ["", "meow"],
+        images: [
+            "https://files.wideunits.nl/memes/examples/cat.png",
+            "https://files.wideunits.nl/memes/examples/rust.jpg",
+        ],
+    },
+];
