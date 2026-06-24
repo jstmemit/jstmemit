e02ce008666ece61020ff407aaf03ae15769cc04
alexcsych <alexsejsi4@gmail.com>
2026-06-24 08:41:52 +0300

    fix: improve absoluteCinema template

--- packages/shared/src/templates/absoluteCinema.tsx
+++ packages/shared/src/templates/absoluteCinema.tsx
@@ -7,9 +7,9 @@ export const absoluteCinema: Template = {
     name: "absoluteCinema",
     width: 1230,
     height: 1422,
-    texts: [],
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 3 }],
     images: [{ id: 0, description: "background" }],
-    element: ({ images }: TemplateProps) => (
+    element: ({ texts, images }: TemplateProps) => (
         <div
             style={{
                 display: "flex",
@@ -20,22 +20,45 @@ export const absoluteCinema: Template = {
             }}
         >
             <img
-                src={images[0]}
-                width={1230}
-                height={1422}
-                style={{
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    objectFit: "cover",
-                }}
-            />
-            <img
                 src="https://files.wideunits.nl/jstmemit/images/templates/absolutecinema.png"
                 width={1230}
                 height={1422}
                 style={{ position: "absolute", top: 0, left: 0 }}
             />
+            <img
+                src={images[0]}
+                width={400}
+                height={400}
+                style={{
+                    position: "absolute",
+                    top: 500,
+                    left: 415,
+                    filter: "grayscale(100%)",
+                    borderRadius: "100%",
+                    objectFit: "cover",
+                }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    left: 0,
+                    top: 100,
+                    width: "100%",
+                    height: "auto",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    fontFamily: "Comic Sans MS",
+                    fontSize: 90,
+                    lineHeight: 1.05,
+                    color: "#ffffff",
+                    WebkitTextStrokeWidth: 6,
+                    WebkitTextStrokeColor: "#000000",
+                }}
+            >
+                {texts[0]}
+            </div>
         </div>
     ),
 };
