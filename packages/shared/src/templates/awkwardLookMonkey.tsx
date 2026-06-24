308018e5ae9a2ec6ea2f4642c4f83240e7fa1709
alexcsych <alexsejsi4@gmail.com>
2026-06-24 17:39:24 +0300

    feat: add awkwardLookMonkey, bearArrest, blooInBed, crowdFromAngryToHappy, diagramIQ, disappointedGuy, disappointedGuy1, disasterGirl, dogAndTeblet meme template files

---
+++ packages/shared/src/templates/awkwardLookMonkey.tsx
@@ -0,0 +1,50 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+
+export const awkwardLookMonkey: Template = {
+    id: 27,
+    name: "awkwardLookMonkey",
+    width: 750,
+    height: 750,
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 12 }],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Impact",
+            }}
+        >
+            <img
+                src="https://files.wideunits.nl/jstmemit/images/templates/awkwardLookMonkey.jpg"
+                width={750}
+                height={750}
+                style={{ position: "absolute", top: 0, left: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    left: 0,
+                    top: 0,
+                    width: "100%",
+                    height: "25%",
+                    padding: "15px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    fontFamily: "Comic Sans MS",
+                    fontSize: 50,
+                    lineHeight: 1.05,
+                    color: "#000000",
+                }}
+            >
+                {texts[0]}
+            </div>
+        </div>
+    ),
+};
---
+++ packages/shared/src/templates/disasterGirl.tsx
@@ -0,0 +1,64 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+
+export const disasterGirl: Template = {
+    id: 19,
+    name: "disasterGirl",
+    width: 2042,
+    height: 1531,
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 8 }],
+    images: [{ id: 0, description: "girl`s face" }],
+    element: ({ texts, images }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Impact",
+            }}
+        >
+            <img
+                src="https://files.wideunits.nl/jstmemit/images/templates/disasterGirl.jpg"
+                width={2042}
+                height={1531}
+                style={{ position: "absolute", top: 0, left: 0 }}
+            />
+            <img
+                src={images[0]}
+                width={1000}
+                height={1000}
+                style={{
+                    position: "absolute",
+                    bottom: 50,
+                    right: 50,
+                    borderRadius: "100%",
+                    objectFit: "cover",
+                }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    left: 0,
+                    top: 0,
+                    width: "100%",
+                    height: "20%",
+                    padding: "15px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    fontFamily: "Comic Sans MS",
+                    fontSize: 120,
+                    lineHeight: 1.05,
+                    color: "#ffffff",
+                    WebkitTextStrokeWidth: 6,
+                    WebkitTextStrokeColor: "#000000",
+                }}
+            >
+                {texts[0]}
+            </div>
+        </div>
+    ),
+};
