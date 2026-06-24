fd945f0a804bc44b380679c809d6c36ad46390be
alexcsych <alexsejsi4@gmail.com>
2026-06-24 09:01:35 +0300

    feat: add stonks1 template

---
+++ packages/shared/src/templates/stonks1.tsx
@@ -0,0 +1,58 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+
+export const stonks1: Template = {
+    id: 15,
+    name: "stonks1",
+    width: 512,
+    height: 386,
+    texts: [{ id: 0, description: "the phrase", minLength: 1, maxLength: 5 }],
+    images: [{ id: 0, description: "stonks guy`s face" }],
+    element: ({ texts, images }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://files.wideunits.nl/jstmemit/images/templates/stonks-without-stonks.jpg"
+                width={512}
+                height={386}
+                style={{ position: "absolute", top: 0, left: 0 }}
+            />
+            <img
+                src={images[0]}
+                width={150}
+                height={150}
+                style={{ position: "absolute", top: 20, left: 40, borderRadius: "100%", objectFit: "cover" }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    bottom: 0,
+                    width: "50%",
+                    height: "50%",
+                    margin: "10px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    fontFamily: "Comic Sans MS",
+                    fontSize: 26,
+                    lineHeight: 1.05,
+                    color: "#ffffff",
+                    WebkitTextStrokeWidth: 4,
+                    WebkitTextStrokeColor: "#000000",
+                }}
+            >
+                {texts[0]}
+            </div>
+        </div>
+    ),
+};
