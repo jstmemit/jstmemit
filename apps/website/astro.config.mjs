0f153de59a1a6b8a12a121d85bde220631882ae0
kamazra4ka <admin@kamazra4ka.nl>
2026-07-03 00:48:01 +0200

    chore: add astro-compress and astro-critters plugins

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -1,17 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import node from '@astrojs/node';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import node from "@astrojs/node";
+import compress from "astro-compress";
+import critters from "astro-critters";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+    vite: {
+        plugins: [tailwindcss()],
+    },
 
-  adapter: node({
-    mode: 'standalone'
-  })
+    adapter: node({
+        mode: "standalone",
+    }),
+
+    integrations: [
+        critters(),
+        compress({
+            CSS: false,
+        }),
+    ],
 });
