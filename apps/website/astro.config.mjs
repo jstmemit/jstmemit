085a3ef0b21e9d0c3c3e71e73cb9bd26bba1e5a1
kamazra4ka <admin@kamazra4ka.nl>
2026-07-04 21:18:02 +0200

    chore: install "og" astro plugin to preview open graph info

--- apps/website/astro.config.mjs
+++ apps/website/astro.config.mjs
@@ -9,6 +9,8 @@ import sitemap from "@astrojs/sitemap";
 
 import robotsTxt from "astro-robots-txt";
 
+import og from "astro-og";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://jstmemit.com",
@@ -23,6 +25,7 @@ export default defineConfig({
     integrations: [
         critters(),
         sitemap(),
+        og(),
         robotsTxt({
             host: true,
         }),
