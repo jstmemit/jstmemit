1745f960ab9d7bfe62e367158bcac7dddf7eb935
kamazra4ka <admin@kamazra4ka.nl>
2026-07-07 20:21:48 +0200

    feat: add a "jstmemit is turned off" warning when trying to make a meme in disabled channel

--- apps/bot/src/interfaces/IComponentsService.ts
+++ apps/bot/src/interfaces/IComponentsService.ts
@@ -2,7 +2,7 @@ import type { ActionRowBuilder, ButtonBuilder } from "discord.js";
 import { type ContainerBuilder } from "discord.js";
 
 export abstract class IComponentsService {
-    public abstract getEnableMessageComponent(isEnabled: boolean, messagesAmount: number): ContainerBuilder;
+    public abstract getEnableMessageComponent(isEnabled: boolean, messagesAmount?: number): ContainerBuilder;
     public abstract getEnableButtonsComponent(isEnabled: boolean): ActionRowBuilder<ButtonBuilder>;
     public abstract getErrorMessageComponent(interactionId: string): ContainerBuilder;
     public abstract getNotEnoughContextMessageComponent(interactionId: string): ContainerBuilder;
