ea0c7dc6ba1069c944213a3a039277e2f1532051
alexcsych <alexsejsi4@gmail.com>
2026-07-09 15:46:24 +0300

    feat: add template topic type into template interface and add topics field

--- packages/shared/src/models/Template.ts
+++ packages/shared/src/models/Template.ts
@@ -2,9 +2,11 @@ import type { ReactNode } from "react";
 import type { TemplateText } from "./TemplateText.ts";
 import type { TemplateImage } from "./TemplateImage.ts";
 import type { TemplateProps } from "./TemplateProps.ts";
+import type { TemplateTopic } from "./TemplateTopic.ts";
 
 export interface Template {
     name: string;
+    topics: TemplateTopic[];
     element: (props: TemplateProps) => ReactNode;
     width: number;
     height: number;
