ad4228352724ac49d7e6c6dd810d57d6a1e593c3
kamazra4ka <admin@kamazra4ka.nl>
2026-07-13 11:05:56 +0200

    feat: add getTemplatesByTopic() method in TemplatesRepository.ts

--- packages/shared/src/interfaces/ITemplatesRepository.ts
+++ packages/shared/src/interfaces/ITemplatesRepository.ts
@@ -1,8 +1,10 @@
 import type { Template } from "#/models/Template.ts";
 import type { TemplateMapKey } from "#/models/TemplateMapKey.ts";
+import type { TemplateTopic } from "#/models/TemplateTopic.ts";
 
 export abstract class ITemplatesRepository {
     public abstract getAll(): Template[];
+    public abstract getTemplatesByTopic(topic: TemplateTopic): string[];
     public abstract getAllByFieldMap<K extends keyof Template>(
         templates: Template[],
         fieldName: K,
