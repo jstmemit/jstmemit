d949e2060fa0105b22d11eef808f5234c1fcce8a
kamazra4ka <admin@kamazra4ka.nl>
2026-07-19 22:31:41 +0200

    docs: add TSdocs to convertSpeechToText method

--- packages/voice/src/services/VoiceService.ts
+++ packages/voice/src/services/VoiceService.ts
@@ -3,10 +3,18 @@ import type { IVoiceService } from "#/interface/IVoiceService.ts";
 export class VoiceService implements IVoiceService {
     private readonly _whisperUrl: string;
 
-    public constructor(whisperUrl: string = "http://whisper:9000/v1/audio/transcriptions") {
+    public constructor(whisperUrl: string = "http://whisper:9000/v1/audio") {
         this._whisperUrl = whisperUrl;
     }
 
+    /**
+     * Fetches audio from url then sends to whisper
+     * for a transcription that returns plain text
+     *
+     * @param url
+     *
+     * @author Kyrylo Maliuha
+     */
     public async convertSpeechToText(url: string): Promise<string> {
         const audio: Response = await fetch(url);
 
@@ -14,7 +22,7 @@ export class VoiceService implements IVoiceService {
         form.append("file", await audio.blob(), "audio.ogg");
         form.append("response_format", "text");
 
-        const res: Response = await fetch(this._whisperUrl, {
+        const res: Response = await fetch(`${this._whisperUrl}/transcriptions`, {
             method: "POST",
             body: form,
         });
