595cb5f613dea5cd26abe164f1c5fc6325a9a624
kamazra4ka <admin@kamazra4ka.nl>
2026-07-20 22:31:12 +0200

    chore: remove files for unused guild events analytics

--- apps/bot/src/events/GuildDelete.ts
+++
@@ -1,7 +0,0 @@
-import { eventsController } from "#/container.ts";
-import { client } from "#/bot.ts";
-import { Events, type Guild } from "discord.js";
-
-client.on(Events.GuildDelete, (guild: Guild) =>
-    eventsController.handleGuildDelete(guild),
-);
