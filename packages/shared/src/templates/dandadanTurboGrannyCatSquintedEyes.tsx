581c4ef336df1c3b55c53f697237f90b281e5f19
alexcsych <alexsejsi4@gmail.com>
2026-07-21 18:25:20 +0300

    feat: add 3 Dandadan meme templates

---
+++ packages/shared/src/templates/dandadanTurboGrannyCatSquintedEyes.tsx
@@ -0,0 +1,67 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const dandadanTurboGrannyCatSquintedEyes: Template = {
+    name: "dandadanTurboGrannyCatSquintedEyes",
+    topics: [Topic.Reaction, Topic.Anime, Topic.Dandadan],
+    types: [Type.textLeftWithBackground],
+    width: 2790,
+    height: 1057,
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 15 }],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://files.wideunits.nl/jstmemit/images/templates/dandadanTurboGrannyCatSquintedEyes.png"
+                width={"50%"}
+                height={1057}
+                style={{ position: "absolute", bottom: 0, right: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    left: 0,
+                    bottom: 0,
+                    width: "50%",
+                    height: "100%",
+                    overflow: "hidden",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                }}
+            >
+                <div
+                    style={{
+                        display: "-webkit-box",
+                        WebkitBoxOrient: "vertical",
+                        WebkitLineClamp: 6,
+                        wordBreak: "break-word",
+                        overflow: "hidden",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 120,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.15em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+        </div>
+    ),
+};
