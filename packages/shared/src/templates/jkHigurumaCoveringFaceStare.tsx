e443b5495f9194365c9229f9ead01bfd4cd8b4ae
alexcsych <alexsejsi4@gmail.com>
2026-07-28 16:58:08 +0300

    feat: add 7 JujutsuKaisen meme templates

---
+++ packages/shared/src/templates/jkHigurumaCoveringFaceStare.tsx
@@ -0,0 +1,96 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const jkHigurumaCoveringFaceStare: Template = {
+    name: "jkHigurumaCoveringFaceStare",
+    topics: [Topic.Reaction, Topic.Anime, Topic.JujutsuKaisen],
+    types: [Type.TwoOption, Type.TextRightWithBackground],
+    width: 446,
+    height: 447,
+    texts: [
+        { id: 0, description: "first option", minLength: 1, maxLength: 8 },
+        { id: 1, description: "second option", minLength: 1, maxLength: 8 },
+    ],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/jkHigurumaCoveringFaceStare.png"
+                width={223}
+                height={447}
+                style={{ position: "absolute", bottom: 0, left: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: 0,
+                    width: "50%",
+                    height: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 6,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 30,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    bottom: 0,
+                    width: "50%",
+                    height: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 6,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 30,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[1]}
+                </div>
+            </div>
+        </div>
+    ),
+};
