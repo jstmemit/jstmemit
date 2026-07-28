efad39f81715f90f876c33f987ce30016f778d2a
alexcsych <alexsejsi4@gmail.com>
2026-07-28 16:58:39 +0300

    feat: add 2 Kon meme templates

---
+++ packages/shared/src/templates/konAzusaNoThanks.tsx
@@ -0,0 +1,45 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const konAzusaNoThanks: Template = {
+    name: "konAzusaNoThanks",
+    topics: [Topic.Reaction, Topic.Anime, Topic.Kon],
+    types: [Type.DefaultText, Type.FaceImage],
+    width: 860,
+    height: 550,
+    texts: [],
+    images: [{ id: 0, description: "Kaguya's face" }],
+    element: ({ images }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/konAzusaNoThanks.png"
+                width={860}
+                height={550}
+                style={{ position: "absolute", bottom: 0, right: 0 }}
+            />
+            <img
+                src={images[0]}
+                width={300}
+                height={300}
+                style={{
+                    position: "absolute",
+                    top: 90,
+                    right: 150,
+                    objectFit: "cover",
+                    borderRadius: "100%",
+                }}
+            />
+        </div>
+    ),
+};
---
+++ packages/shared/src/templates/konAzusaNoThanks1.tsx
@@ -0,0 +1,74 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const konAzusaNoThanks1: Template = {
+    name: "konAzusaNoThanks1",
+    topics: [Topic.Reaction, Topic.Anime, Topic.Kon],
+    types: [Type.DefaultText, Type.FaceImage],
+    width: 860,
+    height: 550,
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 6 }],
+    images: [{ id: 0, description: "Kaguya's face" }],
+    element: ({ texts, images }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/konAzusaNoThanks1.png"
+                width={860}
+                height={550}
+                style={{ position: "absolute", bottom: 0, right: 0 }}
+            />
+            <img
+                src={images[0]}
+                width={300}
+                height={300}
+                style={{
+                    position: "absolute",
+                    top: 90,
+                    right: 150,
+                    objectFit: "cover",
+                    borderRadius: "100%",
+                }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    left: "8%",
+                    top: "15%",
+                    width: "27%",
+                    height: "55%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 5,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+        </div>
+    ),
+};
