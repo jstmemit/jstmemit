1e238d38bb18c621dc61e9fa6b032d2d4b138045
alexcsych <alexsejsi4@gmail.com>
2026-07-29 16:06:34 +0300

    feat: add 4 Shirobako meme templates

---
+++ packages/shared/src/templates/shirobakoWorkStress.tsx
@@ -0,0 +1,131 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const shirobakoWorkStress: Template = {
+    name: "shirobakoWorkStress",
+    topics: [Topic.Reaction, Topic.Movies, Topic.Shirobako],
+    types: [Type.ThreeOption, Type.TextRightWithBackground],
+    width: 952,
+    height: 794,
+    texts: [
+        { id: 0, description: "first option", minLength: 1, maxLength: 8 },
+        { id: 1, description: "second option", minLength: 1, maxLength: 8 },
+        { id: 2, description: "third option", minLength: 1, maxLength: 8 },
+    ],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/shirobakoWorkStress.png"
+                width={476}
+                height={794}
+                style={{ position: "absolute", bottom: 0, left: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: 0,
+                    width: "50%",
+                    height: "33%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                    borderBottom: "4px solid #000000",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 4,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: "33%",
+                    width: "50%",
+                    height: "34%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                    borderBottom: "6px solid #000000",
+                    borderTop: "3px solid #000000",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 4,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[1]}
+                </div>
+            </div>
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    bottom: 0,
+                    width: "50%",
+                    height: "33%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                    borderTop: "1px solid #000000",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 4,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[2]}
+                </div>
+            </div>
+        </div>
+    ),
+};
---
+++ packages/shared/src/templates/shirobakoWorkStress3.tsx
@@ -0,0 +1,98 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const shirobakoWorkStress3: Template = {
+    name: "shirobakoWorkStress3",
+    topics: [Topic.Reaction, Topic.Movies, Topic.Shirobako],
+    types: [Type.TwoOption, Type.TextRightWithBackground],
+    width: 950,
+    height: 528,
+    texts: [
+        { id: 0, description: "first option", minLength: 1, maxLength: 8 },
+        { id: 1, description: "second option", minLength: 1, maxLength: 8 },
+    ],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/shirobakoWorkStress3.png"
+                width={475}
+                height={528}
+                style={{ position: "absolute", bottom: 0, left: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: 0,
+                    width: "50%",
+                    height: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                    borderBottom: "6px solid #000000",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 4,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    bottom: 0,
+                    width: "50%",
+                    height: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                    borderTop: "1px solid #000000",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 4,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[1]}
+                </div>
+            </div>
+        </div>
+    ),
+};
