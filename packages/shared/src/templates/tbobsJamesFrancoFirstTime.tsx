eaef0fb01dae23d074943083bbb508089e823843
alexcsych <alexsejsi4@gmail.com>
2026-07-29 16:06:43 +0300

    feat: add 1 TheBalladOfBusterScruggs meme template

---
+++ packages/shared/src/templates/tbobsJamesFrancoFirstTime.tsx
@@ -0,0 +1,62 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const tbobsJamesFrancoFirstTime: Template = {
+    name: "tbobsJamesFrancoFirstTime",
+    topics: [Topic.Reaction, Topic.Movies, Topic.TheBalladOfBusterScruggs],
+    types: [Type.DefaultText, Type.FaceImage],
+    width: 1200,
+    height: 1296,
+    texts: [],
+    images: [
+        { id: 0, description: "James Franco`s face" },
+        { id: 1, description: "Old man face" },
+    ],
+    element: ({ images }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/tbobsJamesFrancoFirstTime.png"
+                width={1200}
+                height={1296}
+                style={{ position: "absolute", bottom: 0, left: 0 }}
+            />
+            <img
+                src={images[0]}
+                width={350}
+                height={350}
+                style={{
+                    position: "absolute",
+                    top: 10,
+                    left: 330,
+                    objectFit: "cover",
+                    borderRadius: "100%",
+                    transform: "rotate(-10deg)",
+                }}
+            />
+            <img
+                src={images[1]}
+                width={350}
+                height={350}
+                style={{
+                    position: "absolute",
+                    bottom: 250,
+                    left: 370,
+                    objectFit: "cover",
+                    borderRadius: "100%",
+                    transform: "rotate(10deg)",
+                }}
+            />
+        </div>
+    ),
+};
