0417831a31f1e3d577e09580aaa4aad213355aa7
alexcsych <alexsejsi4@gmail.com>
2026-07-29 16:05:03 +0300

    feat: add 1 MyHeroicAcademy meme template

---
+++ packages/shared/src/templates/mhaDekuAllMightFace.tsx
@@ -0,0 +1,63 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const mhaDekuAllMightFace: Template = {
+    name: "mhaDekuAllMightFace",
+    topics: [Topic.Reaction, Topic.Anime, Topic.MyHeroicAcademy],
+    types: [Type.TextTopWithBackground],
+    width: 1200,
+    height: 668,
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 10 }],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/mhaDekuAllMightFace.png"
+                width={1200}
+                height={668}
+                style={{ position: "absolute", bottom: 0, right: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    left: 0,
+                    top: 0,
+                    width: "100%",
+                    height: "150px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 2,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+        </div>
+    ),
+};
