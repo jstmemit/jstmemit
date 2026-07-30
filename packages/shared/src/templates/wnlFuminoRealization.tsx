e11f6fa988e8199e5d17eb9c89752001b3194e9d
alexcsych <alexsejsi4@gmail.com>
2026-07-30 18:51:45 +0300

    feat: add 5 WeNeverLearn meme template

---
+++ packages/shared/src/templates/wnlFuminoRealization.tsx
@@ -0,0 +1,158 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const wnlFuminoRealization: Template = {
+    name: "wnlFuminoRealization",
+    topics: [Topic.Reaction, Topic.Movies, Topic.WeNeverLearn],
+    types: [Type.FourOption, Type.TextRightWithBackground],
+    width: 1088,
+    height: 1237,
+    texts: [
+        { id: 0, description: "first option", minLength: 1, maxLength: 12 },
+        { id: 1, description: "second option", minLength: 1, maxLength: 12 },
+        { id: 2, description: "third option", minLength: 1, maxLength: 12 },
+        { id: 3, description: "fourth option", minLength: 1, maxLength: 12 },
+    ],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/wnlFuminoRealization.png"
+                width={544}
+                height={1237}
+                style={{ position: "absolute", bottom: 0, left: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: 0,
+                    width: "50%",
+                    height: "25%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "black",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 5,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#ffffff",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: "25%",
+                    width: "50%",
+                    height: "25%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "black",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 5,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#ffffff",
+                    }}
+                >
+                    {texts[1]}
+                </div>
+            </div>
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: "50%",
+                    width: "50%",
+                    height: "25%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "black",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 5,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#ffffff",
+                    }}
+                >
+                    {texts[2]}
+                </div>
+            </div>
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: "75%",
+                    width: "50%",
+                    height: "25%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "black",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 5,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#ffffff",
+                    }}
+                >
+                    {texts[3]}
+                </div>
+            </div>
+        </div>
+    ),
+};
---
+++ packages/shared/src/templates/wnlFuminoRealization2.tsx
@@ -0,0 +1,96 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const wnlFuminoRealization2: Template = {
+    name: "wnlFuminoRealization2",
+    topics: [Topic.Reaction, Topic.Anime, Topic.WeNeverLearn],
+    types: [Type.TwoOption, Type.TextRightWithBackground],
+    width: 1090,
+    height: 608,
+    texts: [
+        { id: 0, description: "first option", minLength: 1, maxLength: 12 },
+        { id: 1, description: "second option", minLength: 1, maxLength: 12 },
+    ],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/wnlFuminoRealization2.png"
+                width={545}
+                height={608}
+                style={{ position: "absolute", bottom: 0, left: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: 0,
+                    width: "50%",
+                    height: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "black",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 5,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#ffffff",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    bottom: 0,
+                    width: "50%",
+                    height: "50%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "black",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 5,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 50,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#ffffff",
+                    }}
+                >
+                    {texts[1]}
+                </div>
+            </div>
+        </div>
+    ),
+};
