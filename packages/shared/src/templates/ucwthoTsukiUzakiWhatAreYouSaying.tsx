fc0bcb6603139d281d45a5bc17793639f6fbdcd7
alexcsych <alexsejsi4@gmail.com>
2026-07-30 18:51:34 +0300

    feat: add 1 UzakiChanWantsToHangOut meme template

---
+++ packages/shared/src/templates/ucwthoTsukiUzakiWhatAreYouSaying.tsx
@@ -0,0 +1,63 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const ucwthoTsukiUzakiWhatAreYouSaying: Template = {
+    name: "ucwthoTsukiUzakiWhatAreYouSaying",
+    topics: [Topic.Reaction, Topic.Anime, Topic.UzakiChanWantsToHangOut],
+    types: [Type.TextTopWithBackground, Type.DefaultText],
+    width: 1283,
+    height: 874,
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 8 }],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/ucwthoTsukiUzakiWhatAreYouSaying.png"
+                width={1283}
+                height={674}
+                style={{ position: "absolute", bottom: 0, left: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: 0,
+                    width: "100%",
+                    height: "200px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 2,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 70,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+        </div>
+    ),
+};
