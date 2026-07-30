8ba8188541422b7e59d8e5c4867fc2e5ba9ca28b
alexcsych <alexsejsi4@gmail.com>
2026-07-30 18:51:12 +0300

    feat: add 3 Toradora meme templates

---
+++ packages/shared/src/templates/toradoraTaigaMorningMisanthropeDemotivator.tsx
@@ -0,0 +1,45 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const toradoraTaigaMorningMisanthropeDemotivator: Template = {
+    name: "toradoraTaigaMorningMisanthropeDemotivator",
+    topics: [Topic.Reaction, Topic.Anime, Topic.Toradora],
+    types: [Type.FaceImage, Type.DefaultText],
+    width: 500,
+    height: 418,
+    texts: [],
+    images: [{ id: 0, description: "Taiga`s face" }],
+    element: ({ images }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/toradoraTaigaMorningMisanthropeDemotivator.png"
+                width={500}
+                height={418}
+                style={{ position: "absolute", bottom: 0, left: 0 }}
+            />
+            <img
+                src={images[0]}
+                width={140}
+                height={140}
+                style={{
+                    position: "absolute",
+                    top: 100,
+                    left: 160,
+                    objectFit: "cover",
+                    borderRadius: "100%",
+                }}
+            />
+        </div>
+    ),
+};
