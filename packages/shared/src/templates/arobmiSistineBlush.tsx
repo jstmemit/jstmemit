a533009c350d9df3675e8d41c2593d84037e319d
alexcsych <alexsejsi4@gmail.com>
2026-07-31 17:11:28 +0300

    feat: add 5 AkashicRecords meme templates

---
+++ packages/shared/src/templates/arobmiSistineBlush.tsx
@@ -0,0 +1,64 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const arobmiSistineBlush: Template = {
+    name: "arobmiSistineBlush",
+    topics: [Topic.Reaction, Topic.Anime, Topic.AkashicRecords],
+    types: [Type.TextRightWithBackground, Type.Animated],
+    width: 1080,
+    height: 483,
+    animationDuration: 220,
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 12 }],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/gifs/arobmiSistineBlush.gif"
+                width={540}
+                height={483}
+                style={{ position: "absolute", bottom: 0, left: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: 0,
+                    width: "50%",
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 7,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 60,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+        </div>
+    ),
+};
