0b998b1335394ab27521e0150b4bdb157b546f7c
alexcsych <alexsejsi4@gmail.com>
2026-07-31 17:12:23 +0300

    feat: add 10 BlendS meme templates

---
+++ packages/shared/src/templates/blendsMafuyuHoshikawaSparkle.tsx
@@ -0,0 +1,63 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+
+export const blendsMafuyuHoshikawaSparkle: Template = {
+    name: "blendsMafuyuHoshikawaSparkle",
+    topics: [Topic.Reaction, Topic.Anime, Topic.BlendS],
+    types: [Type.TextTopWithBackground],
+    width: 1280,
+    height: 900,
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 12 }],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55/https://files.wideunits.nl/jstmemit/images/templates/blendsMafuyuHoshikawaSparkle.png"
+                width={1280}
+                height={720}
+                style={{ position: "absolute", bottom: 0, right: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    left: 0,
+                    top: 0,
+                    width: "100%",
+                    height: "180px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "15px",
+                    backgroundColor: "white",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 2,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 60,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+        </div>
+    ),
+};
