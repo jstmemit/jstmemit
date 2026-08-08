c853499483de9b68ec35b4e54d6c7555d63df024
alexcsych <alexsejsi4@gmail.com>
2026-08-08 22:39:35 +0300

    feat: add 11 GabrielDropOut meme templates

---
+++ packages/shared/src/templates/gdoSataniaLaughing1.tsx
@@ -0,0 +1,84 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+import { buildLocales } from "@jstmemit/i18n/helpers/buildLocales";
+import { Locale } from "discord.js";
+
+export const gdoSataniaLaughing1: Template = {
+    name: "gdoSataniaLaughing1",
+    displayName: buildLocales("Satania laughing 1", {
+        [Locale.Russian]: "Сатания смеется 1",
+        [Locale.Ukrainian]: "Сатанія сміється 1",
+        [Locale.Dutch]: "Satania lacht 1",
+        [Locale.French]: "Satania qui rit 1",
+        [Locale.German]: "Satania lacht 1",
+        [Locale.Polish]: "Satania się śmieje 1",
+        [Locale.SpanishES]: "Satania riendo 1",
+        [Locale.SpanishLATAM]: "Satania riendo 1",
+        [Locale.PortugueseBR]: "Satania rindo 1",
+        [Locale.Turkish]: "Satania gülüyo 1r",
+        [Locale.Italian]: "Satania che ride 1",
+        [Locale.Indonesian]: "Satania tertawa 1",
+        [Locale.Czech]: "Satania se směje 1",
+        [Locale.Japanese]: "笑うサターニャ 1",
+        [Locale.Korean]: "웃는 사타냐 1",
+        [Locale.ChineseCN]: "大笑的萨塔妮亚 1",
+    }),
+    topics: [Topic.Reaction, Topic.Anime, Topic.GabrielDropOut],
+    types: [Type.TextTopWithBackground, Type.Animated],
+    width: 500,
+    height: 374,
+    animationDuration: 2250,
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 8 }],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55,onerror=redirect/https://files.wideunits.nl/jstmemit/images/templates/gifs/gdoSataniaLaughing1.gif"
+                width={500}
+                height={284}
+                style={{ position: "absolute", bottom: 0, left: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: 0,
+                    width: "100%",
+                    height: "90px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "10px",
+                    backgroundColor: "white",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 2,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 30,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+        </div>
+    ),
+};
