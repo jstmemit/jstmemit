b946242980c8d3f4d82c7ac0bc8557b465b48670
alexcsych <alexsejsi4@gmail.com>
2026-08-08 22:39:22 +0300

    feat: add 6 Frieren meme templates

---
+++ packages/shared/src/templates/frierenFernFrustratedFrom671.tsx
@@ -0,0 +1,84 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+import { buildLocales } from "@jstmemit/i18n/helpers/buildLocales";
+import { Locale } from "discord.js";
+
+export const frierenFernFrustratedFrom671: Template = {
+    name: "frierenFernFrustratedFrom671",
+    displayName: buildLocales("Frustrated Fern from 67 1", {
+        [Locale.Russian]: "Разочарованная Ферн из-за 67 1",
+        [Locale.Ukrainian]: "Розчарована Ферн через 67 1",
+        [Locale.Dutch]: "Gefrustreerde Fern door 67 1",
+        [Locale.French]: "Fern frustrée à cause de 67 1",
+        [Locale.German]: "Frustrierte Fern wegen 67 1",
+        [Locale.Polish]: "Sfrustrowana Fern przez 67 1",
+        [Locale.SpanishES]: "Fern frustrada por 67 1",
+        [Locale.SpanishLATAM]: "Fern frustrada por 67 1",
+        [Locale.PortugueseBR]: "Fern frustrada por causa de 67 1",
+        [Locale.Turkish]: "67 yüzünden hüsrana uğramış Fern 1",
+        [Locale.Italian]: "Fern frustrata a causa di 67 1",
+        [Locale.Indonesian]: "Fern frustrasi karena 67 1",
+        [Locale.Czech]: "Frustrovaná Fern kvůli 67 1",
+        [Locale.Japanese]: "67のせいで不満なフェルン 1",
+        [Locale.Korean]: "67 때문에 불만스러운 페른 1",
+        [Locale.ChineseCN]: "因为67而沮丧的费伦 1",
+    }),
+    topics: [Topic.Reaction, Topic.Anime, Topic.Frieren],
+    types: [Type.TextTopWithBackground, Type.Animated],
+    width: 811,
+    height: 920,
+    animationDuration: 500,
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 8 }],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55,onerror=redirect/https://files.wideunits.nl/jstmemit/images/templates/gifs/frierenFernFrustratedFrom67.gif"
+                width={811}
+                height={720}
+                style={{ position: "absolute", bottom: 0, left: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    right: 0,
+                    top: 0,
+                    width: "100%",
+                    height: "200px",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "10px",
+                    backgroundColor: "white",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 2,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 60,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+        </div>
+    ),
+};
---
+++ packages/shared/src/templates/frierenWarming.tsx
@@ -0,0 +1,84 @@
+import type { TemplateProps } from "#/models/TemplateProps.ts";
+import type { Template } from "#/models/Template.ts";
+import * as React from "react";
+import { Topic } from "#/models/TemplateTopic.ts";
+import { Type } from "#/models/TemplateType.ts";
+import { buildLocales } from "@jstmemit/i18n/helpers/buildLocales";
+import { Locale } from "discord.js";
+
+export const frierenWarming: Template = {
+    name: "frierenWarming",
+    displayName: buildLocales("Frieren warming up", {
+        [Locale.Russian]: "Фрирен греется",
+        [Locale.Ukrainian]: "Фрірен гріється",
+        [Locale.Dutch]: "Frieren warmt zich op",
+        [Locale.French]: "Frieren se réchauffe",
+        [Locale.German]: "Frieren wärmt sich auf",
+        [Locale.Polish]: "Frieren się ogrzewa",
+        [Locale.SpanishES]: "Frieren calentándose",
+        [Locale.SpanishLATAM]: "Frieren calentándose",
+        [Locale.PortugueseBR]: "Frieren se aquecendo",
+        [Locale.Turkish]: "Frieren ısınıyor",
+        [Locale.Italian]: "Frieren che si scalda",
+        [Locale.Indonesian]: "Frieren menghangatkan diri",
+        [Locale.Czech]: "Frieren se ohřívá",
+        [Locale.Japanese]: "暖まるフリーレン",
+        [Locale.Korean]: "몸을 녹이는 프리렌",
+        [Locale.ChineseCN]: "芙莉莲取暖",
+    }),
+    topics: [Topic.Reaction, Topic.Anime, Topic.Frieren],
+    types: [Type.TextLeftWithBackground, Type.Animated],
+    width: 996,
+    height: 479,
+    animationDuration: 1960,
+    texts: [{ id: 0, description: "caption", minLength: 1, maxLength: 10 }],
+    images: [],
+    element: ({ texts }: TemplateProps) => (
+        <div
+            style={{
+                display: "flex",
+                position: "relative",
+                width: "100%",
+                height: "100%",
+                fontFamily: "Comic Sans MS",
+            }}
+        >
+            <img
+                src="https://wideunits.nl/cdn-cgi/image/f=webp,q=55,onerror=redirect/https://files.wideunits.nl/jstmemit/images/templates/gifs/frierenWarming.gif"
+                width={498}
+                height={479}
+                style={{ position: "absolute", bottom: 0, right: 0 }}
+            />
+            <div
+                style={{
+                    position: "absolute",
+                    left: 0,
+                    top: 0,
+                    width: "50%",
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    textAlign: "center",
+                    padding: "10px",
+                    backgroundColor: "white",
+                }}
+            >
+                <div
+                    style={{
+                        lineClamp: 6,
+                        wordBreak: "break-word",
+                        textOverflow: "ellipsis",
+                        fontFamily: "Comic Sans MS",
+                        fontSize: 65,
+                        lineHeight: 1.05,
+                        paddingBottom: "0.2em",
+                        color: "#000000",
+                    }}
+                >
+                    {texts[0]}
+                </div>
+            </div>
+        </div>
+    ),
+};
