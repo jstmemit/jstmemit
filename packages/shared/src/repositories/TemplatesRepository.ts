2f7ad7178a4120bb6947791a6fd9898bb27baf9b
alexcsych <alexsejsi4@gmail.com>
2026-08-21 15:49:25 +0300

    feat: add 23 new meme templates to TemplatesRepository

--- packages/shared/src/repositories/TemplatesRepository.ts
+++ packages/shared/src/repositories/TemplatesRepository.ts
@@ -479,6 +479,29 @@ import { jkSukunaTransformation } from "#/templates/jkSukunaTransformation.tsx";
 import { kaguyaKaguyaAngry } from "#/templates/kaguyaKaguyaAngry.tsx";
 import { kaguyasamaChikaDancing } from "#/templates/kaguyasamaChikaDancing.tsx";
 import { kaguyasamaChikaDetectiveSmile } from "#/templates/kaguyasamaChikaDetectiveSmile.tsx";
+import { kaguyasamaChikaExcited } from "#/templates/kaguyasamaChikaExcited.tsx";
+import { kaguyasamaChikaGiggling } from "#/templates/kaguyasamaChikaGiggling.tsx";
+import { kaguyasamaChikaHappy } from "#/templates/kaguyasamaChikaHappy.tsx";
+import { kaguyasamaChikaHeartEyes } from "#/templates/kaguyasamaChikaHeartEyes.tsx";
+import { kaguyasamaChikaHittingIshigami } from "#/templates/kaguyasamaChikaHittingIshigami.tsx";
+import { kaguyasamaChikaHorrified } from "#/templates/kaguyasamaChikaHorrified.tsx";
+import { kaguyasamaChikaPouting } from "#/templates/kaguyasamaChikaPouting.tsx";
+import { kaguyasamaHayasakaShy } from "#/templates/kaguyasamaHayasakaShy.tsx";
+import { kaguyasamaHayasakaUnimpressed } from "#/templates/kaguyasamaHayasakaUnimpressed.tsx";
+import { kaguyasamaKaguyaCatEars } from "#/templates/kaguyasamaKaguyaCatEars.tsx";
+import { kaguyasamaKaguyaCryingPout } from "#/templates/kaguyasamaKaguyaCryingPout.tsx";
+import { kaguyasamaKaguyaExcided } from "#/templates/kaguyasamaKaguyaExcided.tsx";
+import { kaguyasamaKaguyaHappy } from "#/templates/kaguyasamaKaguyaHappy.tsx";
+import { kaguyasamaKaguyaIntenseBlush } from "#/templates/kaguyasamaKaguyaIntenseBlush.tsx";
+import { kaguyasamaKaguyaLaughing } from "#/templates/kaguyasamaKaguyaLaughing.tsx";
+import { kaguyasamaKaguyaNervousTea } from "#/templates/kaguyasamaKaguyaNervousTea.tsx";
+import { kaguyasamaKaguyaPanicking } from "#/templates/kaguyasamaKaguyaPanicking.tsx";
+import { kaguyasamaKaguyaRomantic } from "#/templates/kaguyasamaKaguyaRomantic.tsx";
+import { kaguyasamaMikoPointingAngry } from "#/templates/kaguyasamaMikoPointingAngry.tsx";
+import { kaguyasamaShiroganeNervous } from "#/templates/kaguyasamaShiroganeNervous.tsx";
+import { kaguyasamaShiroganeScared } from "#/templates/kaguyasamaShiroganeScared.tsx";
+import { konMugiExcited } from "#/templates/konMugiExcited.tsx";
+import { konMugiPointing } from "#/templates/konMugiPointing.tsx";
 
 export class TemplatesRepository implements ITemplatesRepository {
     /**
@@ -987,6 +1010,29 @@ export class TemplatesRepository implements ITemplatesRepository {
             kaguyaKaguyaAngry,
             kaguyasamaChikaDancing,
             kaguyasamaChikaDetectiveSmile,
+            kaguyasamaChikaExcited,
+            kaguyasamaChikaGiggling,
+            kaguyasamaChikaHappy,
+            kaguyasamaChikaHeartEyes,
+            kaguyasamaChikaHittingIshigami,
+            kaguyasamaChikaHorrified,
+            kaguyasamaChikaPouting,
+            kaguyasamaHayasakaShy,
+            kaguyasamaHayasakaUnimpressed,
+            kaguyasamaKaguyaCatEars,
+            kaguyasamaKaguyaCryingPout,
+            kaguyasamaKaguyaExcided,
+            kaguyasamaKaguyaHappy,
+            kaguyasamaKaguyaIntenseBlush,
+            kaguyasamaKaguyaLaughing,
+            kaguyasamaKaguyaNervousTea,
+            kaguyasamaKaguyaPanicking,
+            kaguyasamaKaguyaRomantic,
+            kaguyasamaMikoPointingAngry,
+            kaguyasamaShiroganeNervous,
+            kaguyasamaShiroganeScared,
+            konMugiExcited,
+            konMugiPointing,
         ];
     }
 
