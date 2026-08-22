39a95970b734b3e5f63d7b41ecc706ba87e64cfa
kamazra4ka <admin@kamazra4ka.nl>
2026-08-22 23:40:43 +0200

    refactor: increase Takumi's cache ceiling for rendered objects to 512MB

--- apps/meme-generator/src/repositories/MemesRepository.ts
+++ apps/meme-generator/src/repositories/MemesRepository.ts
@@ -14,7 +14,7 @@ export class MemesRepository implements IMemesRepository {
 
     public constructor(fontsService: IFontsService) {
         this._fontsService = fontsService;
-        this._renderer = new Renderer({ cacheMaxBytes: 128 * 1024 * 1024 });
+        this._renderer = new Renderer({ cacheMaxBytes: 512 * 1024 * 1024 });
         this._fetchCache = new Map<string, Promise<ArrayBuffer>>();
         this._fontsReady = this._registerFonts();
     }
